1dc2d2eb113ff690f4cb744b3b403a57ecc8c3b5
visualstudio <visualstudio@230-PC20>
2019-01-31 14:33:36 -0500

    2:33

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31jan2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        class Village
+        {
+            public static int numberofvillages = 0;
+            public Village nextvillage;
+            public Village previousvillage;
+            public string Villagename;
+            public bool isAstrildeHere = false;
+            public Village()
+            { Village.numberofvillages++; }
+        }
+        class Countryside
+        {
+            public Village Maple;
+
+            public Village Toronto;
+            public Village Ajax;
+            public void MapInitializer()
+            {
+                Maple = new Village();
+                Maple.Villagename = "Maple";
+                Maple.previousvillage = null;
+                Toronto = new Village();
+                Toronto.Villagename = "Toronto";
+                Toronto.previousvillage = null;
+                Ajax = new Village();
+                Ajax.Villagename = "Ajax";
+                Ajax.nextvillage = null;
+                Ajax.previousvillage = Toronto;
+                Ajax.isAstrildeHere = true;
+
+
+            }
+        }
+    }
+}
